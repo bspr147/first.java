23b5cd19df31eca992a106eb7505a2e912dee4d9
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-27 10:02:04 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+  {
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  if(n/2==0)
+  {
+  System.out.print("even");
+  }
+  else if(n/2!=0)
+  {
+  System.out.print("odd");
+  
+ } 
+ else
+ {
+ System.out.print("invalid");
+ }
+ }
+ }

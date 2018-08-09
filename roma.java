9bae221eede9cdd1e72c598db6fe4627d1d85bec
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-08-09 20:28:04 +0530

    Create roma.java

---
+++ roma.java
@@ -0,0 +1,99 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	String w=s.next();
+	String a="I";
+		String b="II";
+			String c="III";
+				String d="IV";
+					String e="V";
+						String f="VI";
+		String g="VII";
+			String h="VIII";
+			String i="IX";
+				String j="X";	
+					String k="XI";
+						String l="XII";
+							String m="XIII";
+								String n="XIV";
+									String o="XV";
+										String p="XVI";
+											String q="XVII";
+												String r="XVIII";
+													String t="IX";
+														String u="XX";
+    
+		if(w==a)
+		System.out.print("1");
+		
+		if(w==b)
+		System.out.print("2");
+						
+		
+		if(w==c)
+		System.out.print("3");
+		
+		if(w==d)
+		System.out.print("4");
+	
+		if(w==e)
+		System.out.print("5");
+		
+		if(w==f)
+		System.out.print("6");
+		
+		if(w==g)
+		System.out.print("7");
+		
+		if(w==h)
+		System.out.print("8");
+		
+		if(w==i)
+		System.out.print("9");
+		
+		if(w==j)
+		System.out.print("10");
+		
+		if(w==k)
+		System.out.print("11");
+		
+		if(w==l)
+		System.out.print("12");
+		
+		if(w==m)
+		System.out.print("13");
+		
+		if(w==n)
+		System.out.print("14");
+		
+		if(w==o)
+		System.out.print("15");
+		
+		if(w==p)
+		System.out.print("16");
+		
+		if(w==q)
+		System.out.print("17");
+		
+		if(w==r)
+		System.out.print("18");
+		
+		if(w==t)
+		System.out.print("19");
+		
+		if(w==u)
+		System.out.print("20");
+						
+						
+	
+	}
+}
